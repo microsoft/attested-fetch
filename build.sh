321dab9e99ed0c6b562c675ed15440450fbd8e36
Ivar (aɪvɑr) <ivar.prudnikov@gmail.com>
2024-04-10 10:31:49 +0100

    Build on GitHub and add CodeQL config (#9)
    * build on GitHub
    
    * build definition sync with scitt repo
    
    * add codeql config
    
    * remove unused azure-pipelines config

--- build.sh
+++ build.sh
@@ -5,12 +5,30 @@
 
 set -ex
 
+CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-RelWithDebInfo}
 PLATFORM=${PLATFORM:-sgx}
+NINJA_FLAGS=${NINJA_FLAGS:-}
 INSTALL_DIR=$(pwd)/dist
 rm -rf "$INSTALL_DIR"
 
-cmake -G Ninja -B build \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+if [ "$PLATFORM" = "sgx" ]; then
+    CC=${CC:-clang-11}
+    CXX=${CXX:-clang++-11}
+elif [ "$PLATFORM" = "virtual" ]; then
+    CC=${CC:-clang-15}
+    CXX=${CXX:-clang++-15}
+else
+    echo "Unknown platform: $PLATFORM, must be 'sgx' or 'virtual'"
+    exit 1
+fi
+
+git submodule sync
+git submodule update --init --recursive
+
+CC="$CC" CXX="$CXX" cmake -G Ninja -B build \
+  -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
   -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
   -DCOMPILE_TARGET="$PLATFORM"
-ninja -C build install
+
+ninja -C build ${NINJA_FLAGS} --verbose
+ninja -C build ${NINJA_FLAGS} install
